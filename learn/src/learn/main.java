32d7e52bf2870e4e14b88eccb52a300b9f587ff7
hacker104 <troyvansleeuwen@gmail.com>
2018-08-06 08:32:18 +0100

    b

---
+++ learn/src/learn/main.java
@@ -0,0 +1,10 @@
+package learn;
+
+import java.util.Scanner;
+
+public class main {
+	public static void main(String[] args) {
+		Scanner h = new Scanner(System.in);
+		System.out.print(h.nextLine());
+	}
+}
